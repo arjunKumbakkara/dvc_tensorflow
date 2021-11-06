7c337508ec3257fb68cab35a41074ad80d63a7a1
arjunkumbakkara <arjunkumbakkara@gmail.com>
2021-11-06 11:32:11 +0530

    base file is ready

---
+++ src/utils/callbacks.py
@@ -0,0 +1,10 @@
+import os,io
+
+
+
+def create_and_save_checkpoint_callback(callbacks_dir,tensorboard_log_dir):
+
+    
+    
+def create_and_save_tensorboard_callback(callbacks_dir,checkpoint_dir):
+    
