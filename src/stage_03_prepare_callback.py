331c72d14e610cb01e6f4127916f21205a3c83c6
arjunkumbakkara <arjunkumbakkara@gmail.com>
2021-11-06 11:05:59 +0530

    Stage 03 prepared

---
+++ src/stage_03_prepare_callback.py
@@ -0,0 +1,40 @@
+from src.utils.all_utils import create_directory, read_yaml
+from src.utils.models import get_VGG_16_model, prepare_model 
+import argparse
+import pandas as pd
+#import sys
+import os
+import shutil
+from tqdm import tqdm
+import logging
+import os
+import io
+
+logging_str= "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir= "logs" #This is us mentioning that we need a logs folder inside the project.
+#not working
+#create_directory([log_dir])
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str,filemode='a')
+full_final_model=None
+
+
+def prepare_callbacks(config_path,params_path):
+    pass
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config","-c",default="config/config.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
+    
+    parsed_args =args.parse_args()
+
+    try:
+        logging.info(" >>>  Starting: Stage 03 prepare model Callbacks")
+        prepare_callbacks(config_path=parsed_args.config,params_path=parsed_args.params)
+        logging.info("Ended: Stage03 Completed and Callbacks stored as binary in artifacts>>>>")
+    except Exception as e:
+        logging.exception(e)
+        raise e
