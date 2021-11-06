f5c22e5a2a7ffff276c9b756d098afa1173165a1
arjunkumbakkara <arjunkumbakkara@gmail.com>
2021-11-06 09:39:53 +0530

    Second Stage: model preparation done

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,12 +1,14 @@
 from src.utils.all_utils import create_directory, read_yaml
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model, prepare_model 
 import argparse
 import pandas as pd
 #import sys
-import o
+import os
 import shutil
 from tqdm import tqdm
 import logging
+import os
+import io
 
 logging_str= "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir= "logs" #This is us mentioning that we need a logs folder inside the project.
@@ -15,9 +17,8 @@ log_dir= "logs" #This is us mentioning that we need a logs folder inside the pro
 os.makedirs(log_dir,exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str,filemode='a')
  
+full_final_model=None
 
-
-def get_VGG_16_model(input_shape: list, base_model_path):
 
 
 def prepare_base_model(config_path,params_path):
@@ -35,9 +36,33 @@ def prepare_base_model(config_path,params_path):
     create_directory([base_model_dir_path])
 
     base_model_path=os.path.join(base_model_dir_path,base_model_name)
+    #getting the base model (Transfer Learning)
     model=get_VGG_16_model(input_shape=params["IMAGE_SIZE"],model_path= base_model_path)
+    #Getting the addon model:
+    
+    full_model = prepare_model(
+        model,
+        CLASSES=params["CLASSES"],
+        freeze_all=False,
+        freeze_till=params["FREEZE_TILL"],
+        learning_rate=params["LEARNING_RATE"]
+        )
+    logging.info(f"full model summary:\n{_log_model_summary(full_model)}")
+    #logging.info(f"{full_model.summary()}")
+    #Now we save the model
+    #Fixing the path for the same
+    update_base_model_path = os.path.join(base_model_dir_path,artifacts["UPDATED_BASE_MODEL_NAME"])
+    
+    model.save(update_base_model_path)
 
-
+def _log_model_summary(model):
+    with io.StringIO() as stream:
+        #This is by far the best way to print the summaries of a model,ie, a list of 
+        #properties . this is conventionally done by looping a for in java, however, here we 
+        #do it using stream. like in java 8+
+        model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+        summary_str=stream.getvalue()
+    return summary_str
 
 
 
---
+++ src/utils/models.py
@@ -0,0 +1,59 @@
+import tensorflow as tf 
+import os 
+import logging
+from tensorflow.python.keras.backend import flatten
+
+
+
+
+
+def get_VGG_16_model(input_shape, model_path):
+    model=tf.keras.applications.vgg16.VGG16(
+        input_shape=input_shape,
+        weights="imagenet",
+        include_top=False
+    )
+    #Saving the model
+    model.save(model_path)
+    logging.info(f"VGG16 base model saved at {model_path}")
+    return model
+
+
+
+def prepare_model(model,CLASSES,freeze_all,freeze_till,learning_rate):
+    if freeze_all:
+        for layer in model.layers:
+            layer.trainable= False 
+    elif ((freeze_till is not None) and (freeze_till> 1 )):
+        for layer in model.layers[:-freeze_till]:# Freeze_till is a number
+            #Freeze-till is '-' or minus because, from the right the list we need reduce.Which means , -1 is last
+            #layer and -2 will leave the extreme last and  NOT train till penultiamte. Making the last layer trainable
+            layer.trainable= False
+
+#Adding our own fully connected Layers.The real learning layeers.
+    flatten_in= tf.keras.layers.Flatten()(model.output)
+    prediction= tf.keras.layers.Dense(
+        units=CLASSES,
+        activation= "softmax"
+    )(flatten_in)
+#What you see above is a functional approach
+#This is similar to model.add(flatten_layer) very much but thats more like an
+#API or sequential approach.
+
+    full_model = tf.keras.models.Model(
+        inputs= model.input,
+        outputs= prediction
+
+    )
+
+    #logging.info("Custom model summary")
+    full_model.compile(
+        optimizer = tf.keras.optimizers.SGD(learning_rate),
+        loss= tf.keras.losses.CategoricalCrossentropy,
+        metrics=["accuracy"]
+    )
+
+    logging.info("Custom model is compiled and is ready to be trained!")
+
+    return model
+
